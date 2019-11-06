c977dd03f771a20bddff2c30df182ff11065be7c
Zahid Ahmed <zahidzahmed@gmail.com>
2019-11-06 16:52:27 +0000

    Update assignment2.c

--- assignment2.c
+++ assignment2.c
@@ -2,86 +2,112 @@
 #include <stdio.h> 
 #include "dev/sht11-sensor.h"
 #include <stdlib.h>
+#include <math.h>
 /*---------------------------------------------------------------------------*/
 PROCESS(fifo_process, "FIFO process");
 AUTOSTART_PROCESSES(&fifo_process);
 
-
-int Buffer_Size = 4;
-int *CircBuffer;
-int Head = -1;
-int Tail = -1;
-int isfull_flag = 0;
-
 PROCESS_THREAD(fifo_process, ev, data)
 {
 	PROCESS_BEGIN();
+	SENSORS_ACTIVATE(sht11_sensor);
 
+	#define Buffer_ELEMENTS 12
+	#define Buffer_SIZE (Buffer_ELEMENTS + 1)
+	int Head, Tail;
+	int Buffer[Buffer_SIZE];
 
-	void pushtoBuffer(int data){
-
-	if((Tail == Buffer_Size -1 && Head == 0)||(Tail + 1 == Head)){
-		printf("\n Buffer is Full");
-	}
-	if(Tail == Buffer_Size -1){
-		Tail = 0;
-	}
-	else {
-		Tail++;
-		CircBuffer[Tail] = data;
-		printf("\n Write to buffer : %d" , CircBuffer[Tail]);
-		if(Head == -1){
-			Head = 0;		
-		}
-	}
-	}
-
-	void popfromBuffer(){
-		
-		if(Head == -1){
-			printf("Circular queue is empty");
-		}
-		data = CircBuffer[Head];
-		printf("\n Item popping= %d",data);
-		CircBuffer[Head] = 0;
-		if(Head == Tail){
-			Head = Tail = -1;		
-		}
-		else{
-			if(Head == Buffer_Size-1){
-			Head = 0;
-			}
-			else
-			{		
-				Head++;
-			}
-			printf("\nDeleted Item = %d",data);
-		
-		}
-	}
-
-	void printBuffer(){
-	int i;
-	printf("\n Circular Buffer Elements \n");
-		for( i=0; i<Buffer_Size; i++){
-			printf("%d\t", CircBuffer[i]);
-		}
-	
-	}
-
-	
-	
-	pushtoBuffer(30);
-	printf(" Head = %d Tail = %d" , Head, Tail);
-	pushtoBuffer(40);
-	printf(" Head = %d Tail = %d" , Head, Tail);
-	popfromBuffer();
-	printf(" Head = %d Tail = %d" , Head, Tail);
-	pushtoBuffer(60);
-	printf(" Head = %d Tail = %d" , Head, Tail);
-	printBuffer();
-
- 	PROCESS_END();
+void InitBuffer(void)
+{
+    Head = Tail = 0;
 }
 
+int BufferPush(int data)
+{
+    if(Head == (( Tail - 1 + Buffer_SIZE) % Buffer_SIZE))
+    {
+        return -1; 
+    }
+
+    Buffer[Head] = data;
+
+    Head = (Head + 1) % Buffer_SIZE;
+
+    return 0; 
+}
+
+int BufferPop(int *old)
+{
+    if(Head == Tail)
+    {
+        return -1; 
+    }
+
+    *old = Buffer[Tail];
+
+    Tail = (Tail + 1) % Buffer_SIZE;
+
+    return 0; 
+}
+
+
+float standard_deviation(int arr[])
+{
+float sum = 0.0, mean, standDev = 0.0;
+float stddev;
+int i;
+for(i=0; i<10; ++i)
+{
+sum += arr[i];
+}
+mean = sum/10;
+for(i=0; i<10; ++i)
+{standDev += pow(arr[i] - mean, 2);}
+stddev = sqrtf(standDev/10);
+return stddev;
+}
+
+
+
+	InitBuffer();
+
+	while(1){
+		int i;
+		float stddev;
+		int stds;
+		int temp_sensor_value = sht11_sensor.value(SHT11_SENSOR_TEMP);
+		int temperature =  0.01*temp_sensor_value - 39.6 ;
+		
+		BufferPush(temperature);
+		
+		if(Head == (( Tail - 1 + Buffer_SIZE) % Buffer_SIZE))
+    		{
+		 printf("\nBuffer Full");
+       		 break; 
+    		}
+
+		for(i=0; i <Buffer_SIZE; i++){
+		printf("\nBuffer Elements %d", Buffer[i]);
+		stddev = standard_deviation(Buffer);
+		stds = (int)stddev;
+		int k = (stddev - stds) * 10000;
+		printf("\nStandard Deviation = %d.%d", stds , k);
+		}
+		
+		if(stds < 30){ //low activity
+			// No interesting activity occured
+			
+		}
+		else if( stds > 30 && stds < 200){ //High activity
+				
+		}
+		else if( stds > 200) { // Extremely high activity
+			// No Aggregation
+		}
+	}
+	
+	
+	
+ 	PROCESS_END();
+}
 
